6792b741e4cacaecf189c5526820bb933991e4ca
liubo <2998739650@qq.com>
2025-04-08 16:19:31 +0800

    feat: 修改认证方式

--- src/auth/guards/api-key.guard.ts
+++ src/auth/guards/api-key.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-
+const API_KEY='$2b$10$OkrFKjxeaPOXW81n3kztJOr/lXwSVK77cuu2w/06ReEM7eGRJKVSG'
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
   constructor(private configService: ConfigService) {}
@@ -8,8 +8,8 @@ export class ApiKeyGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
     const apiKey = request.headers['x-api-key'];
-    const validApiKey = this.configService.get<string>('API_KEY');
-
+    const validApiKey = API_KEY;
+    
     return apiKey === validApiKey;
   }
 }
